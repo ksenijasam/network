9a32087f56fd81754bf068215d1329015e251282
ksenijasam <ksenija2702@gmail.com>
2023-06-10 17:33:07 +0200

    admin registration

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Post, UserProfile, Following, Like
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(UserProfile)
+admin.site.register(Following)
+admin.site.register(Like)
