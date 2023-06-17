35df5746459a254e5defad30f1a5fe9719c7d8ff
ksenijasam <ksenija2702@gmail.com>
2023-06-17 16:12:53 +0200

    django pagination

--- network/views.py
+++ network/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.core import serializers
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from django.http import HttpResponse, JsonResponse, Http404
 
@@ -22,9 +23,16 @@ def index(request):
         posts_with_likes_count = Post.objects.annotate(likes_count=Count('liked_post')).order_by('-date_time')
         all_posts = posts_with_likes_count.values('id', 'user__username', 'user__pk', 'content', 'date_time', 'likes_count')
 
-        return render(request, "network/index.html", {
-            'all_posts': all_posts,
-        })
+        paginator = Paginator(all_posts, 10)
+
+        page_number = request.GET.get('page', '1')  
+        page = paginator.get_page(page_number)
+
+        context = {
+            'page': page,
+        }
+
+        return render(request, 'network/index.html', context)
     except:
         return render(request, "network/index.html")
 
